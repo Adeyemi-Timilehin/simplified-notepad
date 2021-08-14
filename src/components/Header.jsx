b55fc28fb4c49cfdb0e3a38753b87ac9763f6fb9
Adeyemi <adeyemitimmy@gmail.com>
2021-08-14 11:27:27 -0700

    initial keeper commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PolymerIcon from '@material-ui/icons/Polymer';
+
+function Header() {
+  return (
+
+    <header>
+      <PolymerIcon />
+      <h1 style={{ display:"inline-block" }}>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
